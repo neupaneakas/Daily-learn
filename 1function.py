fb2d8760d6080029b2d3eec27ce4995852ef766f
Aakash Neupane <neupaneakash687@gmail.com>
2025-12-27 19:48:54 +0545

    function again

--- 1function.py
+++ 1function.py
@@ -1,5 +1,28 @@
-a= 40
-b= 50
-c=90
-average = (a+b+c)/3
-print(average)
+# a= 40
+# b= 50
+# c=90
+# average = (a+b+c)/3
+# print(average)
+def avg ():
+    a = int(input("Enter a number: "))
+    b = int(input("Enter a number: "))
+    c = int(input("Enter a number: "))
+    average = (a+b+c)/3
+    print(average)
+
+avg()
+avg()
+avg()
+avg()
+avg()
+avg()
+
+
+
+
+
+
+
+
+
+
